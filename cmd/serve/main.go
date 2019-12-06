cb67d508851e468c6f9d5fe6f01bd8e4c815d58e
Aditi <aditi@lob.com>
2019-12-06 11:24:49 -0800

    feat(server): add base server with health checks

---
+++ cmd/serve/main.go
@@ -0,0 +1,24 @@
+// cmd/serve/main.go
+package main
+
+import (
+    "net/http"
+
+    "github.com/sudoku-lord/goyagi/pkg/server"
+    "github.com/lob/logger-go"
+)
+
+func main() {
+    log := logger.New()
+
+    srv := server.New()
+
+    log.Info("server started")
+
+    err := srv.ListenAndServe()
+    if err != nil && err != http.ErrServerClosed {
+        log.Err(err).Fatal("server stopped")
+    }
+
+    log.Info("server stopped")
+}
